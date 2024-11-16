cf25ac36601a5afa236044c7c06d67d61ce37d05
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2024-11-16 22:12:59 +0530

    Default rating fixed: Game Draw

--- common/types/GameHistory.go
+++ common/types/GameHistory.go
@@ -8,7 +8,7 @@ type GameHistory struct {
 	UserID             string    `json:"user_id" gorm:"index"`
 	OpponentUserID     string    `json:"opponent_user_id" gorm:"index"`
 	OpponentUsername   string    `json:"username" gorm:"index"`
-	RatingChange       int       `json:"rating_change" gorm:"index;default:1000"`
+	RatingChange       int       `json:"rating_change" gorm:"index;default:0"`
 	RatingBeforeChange int       `json:"rating_before_change" gorm:"index;default:1000"`
 	CreatedAt          time.Time `json:"createdAt" gorm:"autoCreateTime"`
 }
