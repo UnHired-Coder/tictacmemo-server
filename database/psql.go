3ca927c6548d083f1cca76bcd694eb447099c4ef
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2024-12-12 12:56:45 +0530

    Revert: Reverts changes related to vercel deployment

--- database/psql.go
+++ database/psql.go
@@ -21,52 +21,42 @@ func GetDatabase() *gorm.DB {
 		dbHost = os.Getenv("TEST_DB_HOST")
 	}
 
-	connectionString := ""
-
 	logMode := logger.Silent
 	if os.Getenv("ENV") == "dev" {
 		logMode = logger.Info
-		connectionString = fmt.Sprintf(
-			"postgres://%s:%s@%s:%s/%s?sslmode=prefer",
-			os.Getenv("DB_USERNAME"),
-			os.Getenv("DB_PASSWORD"),
-			dbHost,
-			os.Getenv("DB_PORT"),
-			os.Getenv("DB_NAME"),
-		)
-	} else {
-		connectionString = "postgres://neondb_owner:AboRXKe7x2SB@ep-gentle-voice-a11e1sd6-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 	}
 
-	log.Println("Databse hosted on url...")
+	connectionString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=prefer",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		dbHost,
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
 	log.Println(connectionString)
 
 	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{
 		Logger: logger.Default.LogMode(logMode),
 	})
 
-	log.Println("Checking connection...")
-
 	if err != nil {
 		log.Fatal("Unable to connect to the database " + err.Error())
 	}
 
 	if sqlDB, err := db.DB(); err == nil {
-		sqlDB.SetConnMaxLifetime(time.Second * 30)
-		sqlDB.SetMaxOpenConns(2)
+		sqlDB.SetConnMaxLifetime(time.Hour)
+		sqlDB.SetMaxOpenConns(100)
 	} else {
 		log.Fatal("Failed to set connection pool parameters")
 	}
-
-	log.Println("Creating table User")
 
 	// User table
 	err = db.AutoMigrate(&types.User{})
 	if err != nil {
 		log.Fatal("Failed to create User table:", err)
 	}
-
-	log.Println("Creating table Game History")
 
 	// Games table
 	err = db.AutoMigrate(&types.GameHistory{})
